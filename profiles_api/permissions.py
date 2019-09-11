274e60ec002f95f8b59f832389f3763c84ed68e9
hayden Bush <haydenbush@haydens-Mac-mini.local>
2019-09-11 15:00:02 -0400

    added user profile feed api

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -10,3 +10,13 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
         
         return obj.id == request.user.id
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow users to edit their own feed items"""
+
+    def has_object_permission(self, request, view, obj):
+        """check user is trying to edit their own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
